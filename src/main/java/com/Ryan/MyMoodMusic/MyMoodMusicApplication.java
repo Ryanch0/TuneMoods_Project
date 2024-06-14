8399d5875f751ba32ca6c61d7cfed86a33ceb21d
Ryan <tjddnfkdls0@naver.com>
2024-06-14 13:06:59 +0900

    first commit

---
+++ src/main/java/com/Ryan/MyMoodMusic/MyMoodMusicApplication.java
@@ -0,0 +1,13 @@
+package com.Ryan.MyMoodMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoodMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoodMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ryan/MyMoodMusic/MyMoodMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ryan.MyMoodMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoodMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
